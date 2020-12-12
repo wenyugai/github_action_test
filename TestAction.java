e690168180f37b0934e6f747887eaacb2ce8b8f0
wenyugai <wenyugai@outlook.com>
2020-12-12 18:27:47 -0500

    uploading src code

---
+++ TestAction.java
@@ -0,0 +1,7 @@
+package github_action_test;
+
+public class TestAction {
+	public static int add(int i,int j) {
+		return i+j;
+	}
+}
---
+++ TestAdd.java
@@ -0,0 +1,15 @@
+package github_action_test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+//import github_action_test.TestAction;
+
+public class TestAdd {
+	
+	@Test
+	void testAdd() {
+		assertEquals(1,TestAction.add(0,1));
+	}
+	
+}
